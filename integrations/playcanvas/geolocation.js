3f43826601c32a1edb6c009940eeb71d1f81dcd0
Will Eastcott <will@playcanvas.com>
2023-07-06 10:48:42 +0100

    Lint fixes

--- integrations/playcanvas/geolocation.js
+++ integrations/playcanvas/geolocation.js
@@ -2,7 +2,7 @@ var Geolocation = pc.createScript('geolocation');
 
 Geolocation.attributes.add('camera', {
     type: 'entity'
-})
+});
 
 Geolocation.prototype.teleport = function (lon, lat, alt) {
     const [x, y, z] = earthatile.geodeticToCartesian(lon, lat, alt ?? 300);
@@ -12,7 +12,7 @@ Geolocation.prototype.teleport = function (lon, lat, alt) {
 };
 
 // initialize code called once per entity
-Geolocation.prototype.initialize = function() {
+Geolocation.prototype.initialize = function () {
     // Create a debug UI
     this.panel = new pcui.Panel({
         collapsible: true,
@@ -25,18 +25,18 @@ Geolocation.prototype.initialize = function() {
     this.longLat = new pcui.VectorInput({
         dimensions: 2,
         readOnly: true
-    })
+    });
     let group = new pcui.LabelGroup({
         text: 'Geodetic Coord',
         field: this.longLat
-    })
+    });
     this.panel.append(group);
 
     const destInput = new pcui.TextInput();
     destInput.on('change', (value) => {
         fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${value}`)
             .then(response => response.json())
-            .then(data => {
+            .then((data) => {
                 if (data.length > 0) {
                     const latitude = data[0].lat;
                     const longitude = data[0].lon;
@@ -50,7 +50,7 @@ Geolocation.prototype.initialize = function() {
     group = new pcui.LabelGroup({
         text: 'Destination',
         field: destInput
-    })
+    });
     this.panel.append(group);
 
     const geoButton = new pcui.Button({
@@ -65,17 +65,17 @@ Geolocation.prototype.initialize = function() {
     group = new pcui.LabelGroup({
         text: '',
         field: geoButton
-    })
+    });
     this.panel.append(group);
 };
 
 // update code called every frame
-Geolocation.prototype.update = function(dt) {
+Geolocation.prototype.update = function (dt) {
     if (this.camera) {
         const pos = this.camera.getPosition().clone();
         const offset = this.entity.getPosition();
         pos.sub(offset);
-        const [lon, lat, alt] = earthatile.cartesianToGeodetic(pos.x, pos.y, pos.z);
+        const [lon, lat] = earthatile.cartesianToGeodetic(pos.x, pos.y, pos.z);
         this.longLat.value = [lon, lat];
     }
 };
